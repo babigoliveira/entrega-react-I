11866c4a01c94fec6e520f8d1360b3c0c7a9accb
Bárbara Oliveira <barbaragdeoliveira@hotmail.com>
2022-06-28 16:30:17 -0300

    desafio finalizado com responsividade

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Footer />
+    </>
+  );
+}
+export default App;
